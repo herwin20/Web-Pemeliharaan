28b06f02b833048731fc6af6040b12f836d5100d
herwin20 <herwinjanuardi20@gmail.com>
2023-07-17 07:51:17 +0700

    Update Juli

--- resources/views/kpi/wrenchtimeinst.blade.php
+++ resources/views/kpi/wrenchtimeinst.blade.php
@@ -194,7 +194,7 @@
                             <div class="dropdown">
                                 <button class="btn p-0" type="button" id="orederStatistics" data-bs-toggle="dropdown"
                                     aria-haspopup="true" aria-expanded="false">
-                                    <i class="bx bx-dots-vertical-rounded"></i>
+                                   <i class="bx bx-dots-vertical-rounded"></i>
                                 </button>
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="orederStatistics">
                                     <a class="dropdown-item" href="javascript:void(0);">Select All</a>
@@ -207,7 +207,7 @@
                             <div class="d-flex justify-content-between align-items-center mb-3">
                                 <div class="d-flex flex-column align-items-center gap-1">
                                     <h2 class="mt-3 mb-2">55%</h2>
-                                    <span>Target Min Wrench Time</span>
+                                      <span>Target Min Wrench Time</span>
                                 </div>
                                 <div class="d-flex flex-column align-items-center gap-1">
                                     <h2 class="text-success mt-3 mb-2"> {{ $fixtotalwrenchtime }} %</h2>
@@ -218,7 +218,7 @@
                                 <li class="d-flex mb-4 pb-1">
                                     <div class="avatar flex-shrink-0 me-3">
                                         <span class="avatar-initial rounded bg-label-primary">
-                                            <i class='bx bx-calendar'></i></span>
+                                        <i class='bx bx-calendar'></i></span>
                                     </div>
                                     <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                                         <div class="me-2">
